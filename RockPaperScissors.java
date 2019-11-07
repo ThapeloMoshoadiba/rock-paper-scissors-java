c4159e41a71fc7d4ba62586479a713527d0fc552
Thapelo Moshoadiba <54323840+ThapeloMoshoadiba@users.noreply.github.com>
2019-11-07 20:55:38 +0200

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,20 +1,3 @@
-/*
-
-04 August 2019
-Thapelo Moshoadiba
-Introduction to programming Task 2.1
-
-Write a program that allows the user to play the rock, paper, scissors game
-? The program should randomly generate a number 0, 1, 2, which represents scissors, rock and paper respectively.
-? The program should then prompt the user to enter a number 0, 1, 2.
-? Once the user has entered their number the program should inform them whether they win, lose or draw.
-? The rules of the game are as follows:
-  ? Scissors beats paper
-  ? Rock beats scissors
-  ? Paper beats rock
-
-*/
-
 package rpsgame;
 
 import java.util.Random;
